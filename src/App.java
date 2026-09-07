c66abc5ce8f6e55135a1d2e07a7d512306ffbc77
Hariom Ingle <hengineer364@gmail.com>
2026-09-07 16:56:16 +0530

    Add the button in button panal

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       calculator calc = new calculator();
+       Calculator calc = new Calculator();
     }
 }
--- src/calculator.java
+++ src/calculator.java
@@ -1,3 +1,64 @@
-public class calculator {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Arrays;
+import javax.swing.border.LineBorder;
 
+
+public class Calculator {
+    int height = 540;
+    int width = 360;
+
+    Color customLightGray = new Color(212, 212, 210);
+    Color customDarkGray = new Color(80, 80, 80);
+    Color customBlack = new Color(28, 28, 28);
+    Color customOrange = new Color(255, 149, 0);
+
+    JFrame frame = new JFrame("Calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
+    JPanel buttonPanel = new JPanel();
+
+    String[] buttonValues = {
+            "AC", "+/-", "%", "÷",
+            "7", "8", "9", "×",
+            "4", "5", "6", "-",
+            "1", "2", "3", "+",
+            "0", ".", "√", "="
+    };
+    String[] rightSymbols = {"÷", "×", "-", "+", "="};
+    String[] topSymbols = {"AC", "+/-", "%"};
+    Calculator() {
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.WHITE);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        frame.add(displayPanel,BorderLayout.NORTH);
+
+        buttonPanel.setLayout( new GridLayout(5,4));
+        buttonPanel.setBackground(customBlack);
+        frame.add(buttonPanel);
+
+        for(int i=0;i<buttonValues.length;i++ ){
+            JButton button=new JButton();
+            String buttonValue=buttonValues[i];
+            button.setFont(new Font("Arial", Font.PLAIN, 80));
+            button.setText(buttonValue);
+            buttonPanel.add(button);
+
+        }
+    }
 }
+
